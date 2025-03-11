a554e55996914316654d5ef3a17dfe167ba47a32
lucianlavric <lucianlavric2005@gmail.com>
2025-03-11 12:21:29 -0400

    supabase backend created with vite

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import supabase from './supabase-client';
+
+function App() {
+// const[]
+
+  return (
+    <div>
+      {" "}
+      <h1>FUNDHOMECARE CANADA</h1>
+      <div>
+        <input type="text" placeholder='New addition'/>
+        <button>Add Item</button>
+      </div>
+
+
+      <ul>
+
+      </ul>
+    </div>
+  )
+  
+}
+
+export default App;
