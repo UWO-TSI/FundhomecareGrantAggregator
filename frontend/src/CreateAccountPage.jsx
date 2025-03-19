e8b19bf94119f3573dc1ea79175d8a05011cd7f9
Adit <aditbhimani007@gmail.com>
2025-03-19 18:42:06 -0400

    Changed up the whole app and converted it to Vite, and got rid of the original react stuff

---
+++ frontend/src/CreateAccountPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../src/styles/AuthPages.css';
+import { Link } from 'react-router-dom';
+
+function CreateAccountPage() {
+    return (
+        <div className="auth-page-container">
+            <h2 className="auth-title">Create Account</h2>
+            <form className="auth-form">
+                <input type="text" placeholder="Full Name" className="auth-input" />
+                <input type="email" placeholder="Email" className="auth-input" />
+                <input type="password" placeholder="Password" className="auth-input" />
+                <input type="password" placeholder="Confirm Password" className="auth-input" />
+                <button type="submit" className="auth-button">Create Account</button>
+            </form>
+            <p className="auth-link">
+                Already have an account? <Link to="/">Sign In</Link>
+            </p>
+        </div>
+    );
+}
+
+export default CreateAccountPage;
