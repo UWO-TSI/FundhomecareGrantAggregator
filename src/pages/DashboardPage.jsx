d980ef74ecdafcc35927144c222a79202c9d9fa2
Justin Rowbotham <jrowbot2@uwo.ca>
2025-04-17 15:58:20 -0400

    WIP 3

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import "../styles/DashboardPage.css";
 import { useNavigate } from "react-router-dom";
